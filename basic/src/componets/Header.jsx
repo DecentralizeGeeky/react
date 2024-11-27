a5e950b6a3bfbaecd92c345ce6746a2eb6ad06da
ADAMU MUHAMMAD MUHAMMAD <muhdadamuhd@gmail.com>
2024-11-27 12:04:33 +0100

    Router

--- basic/src/componets/Header.jsx
+++ basic/src/componets/Header.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <header>
+    <header className="header">
       <nav>
         <ul>
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/services">Services</a>
+            <Link to="/services">Services</Link>
           </li>
           <li>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
